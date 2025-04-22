fd9bf8ed5cd07c8bdbd19d6b89959ce014f6f6e7
ucup233 <sufhidayat9@gmail.com>
2025-04-22 16:09:20 +0700

    first commit

---
+++ assets/jsCHS.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video Red Velvet");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio Red Velvet");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar Red Velvet");
+});
